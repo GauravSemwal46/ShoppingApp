77b3ef884cf11aaf6fe485830f6be1b109305e4e
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-06 10:36:44 +0530

    create UI for the onboarding screen

--- Shopping App/Shopping App/OnboardingScreen.swift
+++ Shopping App/Shopping App/OnboardingScreen.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct OnboardingScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 30) {
+                Spacer()
+                Image("img")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .padding()
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Order your Favorite Fruits")
+                        .fontWeight(.bold)
+                        .font(.system(.largeTitle))
+                    Text("Eat fresh fruits and try to be healthy")
+                        .font(.system(.title3))
+                        .foregroundColor(.black.opacity(0.7))
+                }.padding()
+                Spacer()
+                NavigationLink(destination: HomeScreen()) {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color("6"))
+                        .frame(width: 280, height: 60, alignment: .trailing)
+                        .overlay(
+                            HStack(spacing: 10) {
+                                Text("Next")
+                                    .font(.system(.title))
+                                    .fontWeight(.bold)
+                                Image(systemName: "chevron.right")
+                            }
+                                .foregroundColor(.black)
+                        )
+                }
+                Spacer()
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
 
