2b3b3ab10b1d9a0e8b5e717cad932257dc06225e
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-11 08:43:11 +0530

    add fruit and places model

---
+++ Shopping App/Shopping App/Models/ImageData.swift
@@ -0,0 +1,20 @@
+//
+//  ImageData.swift
+//  Shopping App
+//
+//  Created by Gaurav Semwal on 11/03/25.
+//
+
+import Foundation
+
+struct FruitModel {
+    var id: Int
+    var title: FruitTitle
+    var image: String
+    var price: String
+    var color: String
+}
+
+enum FruitTitle: String {
+    case apple, apricot, avacado, banana, blackberry, cherry, dragonfruit, fig, grapes, kiwi, lemon, mango, muskmelon, orange, papaya, pear, pineapple, plum, raspberry, watermelon
+}
---
+++ Shopping App/Shopping App/Models/NearYouData.swift
@@ -0,0 +1,16 @@
+//
+//  NearYouData.swift
+//  Shopping App
+//
+//  Created by Gaurav Semwal on 11/03/25.
+//
+
+import Foundation
+
+struct PlacesModel {
+    var id: Int
+    var name: String
+    var time: String
+    var rating: String
+    var image: String
+}
